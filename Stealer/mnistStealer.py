a62ea8b4873c033ecf073841a912b0f476040228
whoami90506 <ChengDeLin@DESKTOP-OINFK84.localdomain>
2018-12-20 21:49:13 +0800

    mnist steal structure

--- Stealer/mnistStealer.py
+++ Stealer/mnistStealer.py
@@ -3,6 +3,29 @@ import os.path
 import sys
 sys.path.insert(1, os.path.join(os.path.dirname(os.path.abspath(__file__)), "pre_model") )
 
+import numpy as np
 import datasets
+import packagedModel as pack
+
+nTrain = 10000
+epoch = 100
+nBatch = nTrain//5
 
 pre_model = datasets.getMnistModel()
+
+x_train, _ , x_test, y_test = datasets.getMnist()
+x_train = x_train[np.random.choice(x_train.shape[0], nTrain , replace=False)]
+y_train = pre_model.predict(x_train)
+
+layers = [("Linear", (784,100)), \
+        ("ReLU", ()), \
+        ("Linear", (100,100)), \
+        ("ReLU", ()), \
+        ("Linear", (100,10)), \
+        ("Softmax", 1)]
+
+model = pack.classifyModel(layers, loss_func="BCELoss", optimArgs = {"lr" : 5e-5})
+model.train(x_train, y_train, epoch, nBatch, printPerEpoch=10)
+model.save(os.path.join(os.path.dirname(os.path.abspath(__file__)), "model/mnist.model"))
+
+print(model.getAccuracy(x_test, y_test))
