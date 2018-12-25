550a04095c8aec6e378856e1a75953aceefcf58c
whoami90506 <whoami90506@github.com>
2018-12-25 20:09:27 +0800

    fix data error

--- Stealer/mnistStealer.py
+++ Stealer/mnistStealer.py
@@ -7,9 +7,10 @@ import numpy as np
 import datasets
 import packagedModel as pack
 
-nTrain = 100
+nTrain = 200
 epoch = 100
 nBatch = 25
+pPerEpoch = 10
 
 print("\n======================== pretrained model========================")
 pre_model = datasets.getMnistModel()
@@ -17,8 +18,8 @@ print(pre_model.model)
 
 x_train, y_train , x_test, y_test = datasets.getMnist()
 mask = np.random.choice(x_train.shape[0], nTrain , replace=False)
-x_train = x_train[np.random.choice(x_train.shape[0], nTrain , replace=False)]
-y_train = y_train[np.random.choice(x_train.shape[0], nTrain , replace=False)]
+x_train = x_train[mask]
+y_train = y_train[mask]
 y_value = pre_model.predict(x_train)
 
 layers = [("Linear", (784,100)), \
@@ -29,18 +30,20 @@ layers = [("Linear", (784,100)), \
         ("Softmax", 1)]
 
 #Origin model
-print("\n======================== copy model ========================")
+print("\n======================== origin model ========================")
 origin = pack.classifyModel(layers, optimArgs = {"lr" : 5e-5})
-origin.train(x_train, y_value, epoch, nBatch, printPerEpoch=epoch//10, yType = "long", yTo2D = False)
+origin.train(x_train, y_train, epoch, nBatch, printPerEpoch=pPerEpoch, yType = "long", yTo2D = False)
 origin.save(os.path.join(os.path.dirname(os.path.abspath(__file__)), "model/origin_mnist.model"))
 
 #copy model
 print("\n======================== copy model ========================")
 copy = pack.classifyModel(layers, loss_func="BCELoss", optimArgs = {"lr" : 5e-5})
-copy.train(x_train, y_value, epoch, nBatch, printPerEpoch=epoch//10)
+copy.train(x_train, y_value, epoch, nBatch, printPerEpoch=pPerEpoch)
 copy.save(os.path.join(os.path.dirname(os.path.abspath(__file__)), "model/copy_mnist.model"))
 
 
 print("\n======================== summary ========================")
-print("Accuracy of origin model :", origin.getAccuracy(x_test, y_test))
-print("Accuracy of   copy model :", copy.getAccuracy(x_test, y_test))
+print("Training Accuracy of origin model :", origin.getAccuracy(x_train, y_train))
+print("Training Accuracy of   copy model :", copy.getAccuracy(x_train, y_train))
+print("Testing  Accuracy of origin model :", origin.getAccuracy(x_test, y_test))
+print("Testing  Accuracy of   copy model :", copy.getAccuracy(x_test, y_test))
