00cb1c5c156d76c4a747c3f6c80f1eabefe0b77f
kuan-hao <ntueeb05howard@gmail.com>
2019-01-10 21:01:31 +0800

    Upload MNIST_test.csv

--- BigML/Utils/MNIST_dataset.py
+++ BigML/Utils/MNIST_dataset.py
@@ -5,7 +5,7 @@ import pickle, gzip
 import csv
 
 
-f = gzip.open('mnist.pkl.gz','rb')
+f = gzip.open('../mnist.pkl.gz','rb')
 train_set, valid_set, test_set = pickle.load(f,encoding='unicode-escape')
 f.close()
 x_train,y_train=train_set[0],train_set[1]
@@ -16,20 +16,45 @@ x_test,y_test=test_set[0],test_set[1]
 print("Shape : ", x_train.shape,y_train.shape,x_test.shape,y_test.shape)
 print("Max and min of x_train and y_train : ", np.min(x_train),np.max(x_train),np.min(y_train),np.max(y_train))
 
-
-# with open("MNIST.csv", "w") as f:
-#     for i in range(0, len(y_train)):
-#         new_train = np.append(x_train[i], y_train[i])
-#         writer = csv.writer(f)
-#         writer.writerow(new_train)
+# write testing answer to file
 
 
+lister = []
+with open("MNIST_test__qaa.csv", "a") as f:
+    for i in range(0,784):
+        lister.append("Field_"+str(i+1))
+    lister.append("Answer")
+    wr = csv.writer(f, quoting=csv.QUOTE_ALL)
+    wr.writerow(lister)
+    for i in range(0,784):
+        tmp = x_test[i].tolist()
+        tmp.append("'" + str(y_test[i]) + "'")
+        wr.writerow(tmp)
+# writer.writerows(x_test)
 
-with open("MNIST_test.csv", "w") as f:
-    writer = csv.writer(f)
-    writer.writerows(x_test)
 
 
+
+
+
+    # lister.append("Field_"+str(i+1))
+
+
+
+# with open("MNIST_test_ans.csv", "a") as f:
+#     f.write("Answer\n")
+#     for item in y_test:
+#         f.write("%s\n" % item)
+
+# lister = []
+# with open("MNIST_test.csv", "a") as f:
+#     for i in range(0,784):
+#         lister.append("Field_"+str(i+1))
+#     lister.append("Answer")
+#     wr = csv.writer(f, quoting=csv.QUOTE_ALL)
+#     wr.writerow(lister)
+#     writer = csv.writer(f)
+#     writer.writerows(x_test)
 
 # print(x_train[0])
 # print(y_train[0])
