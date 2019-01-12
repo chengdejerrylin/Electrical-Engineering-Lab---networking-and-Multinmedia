5975c78d7edb1702156de30fe38e12041d1cf845
whoami90506 <whoami90506@github.com>
2019-01-12 16:04:18 +0800

    data

---
+++ Stealer/getCsvAverage.py
@@ -0,0 +1,50 @@
+from argparse import ArgumentParser
+import csv
+
+parser = ArgumentParser(description='A program that get the average of the eperiment data.')
+parser.add_argument('inputFile', help='input csv file.')
+parser.add_argument('-n', dest='nAcc',default=4, help='# of accuracy.')
+
+args = parser.parse_args()
+outputFile = ''.join(args.inputFile.split('.')[:-1]) + '_average.csv'
+
+#output
+isTitle = True
+current = None
+num = 0
+acc = []
+for i in range(args.nAcc) : acc.append(0.0)
+
+
+with open(args.inputFile, 'r', newline='') as ifs :
+	data = csv.reader(ifs)
+	with open(outputFile, 'w', newline='') as os :
+		writer = csv.writer(os, delimiter=',')
+
+		for row in data :
+			if isTitle :
+				writer.writerow(row)
+				isTitle = False
+				continue
+
+			if current is None : current = list(row)
+			for i in range(len(row) - args.nAcc) :
+				if current[i] != row[i] :
+					if num != 0 :
+						for j in range(args.nAcc) : current[-args.nAcc +j] = acc[j]/num
+						if current[-1] > current[-2] : print(current)
+						writer.writerow(current)
+
+						current = list(row)
+						num = 0
+						for j in range(args.nAcc) : acc[j] = 0.0
+					break
+
+			for i in range(args.nAcc) : acc[i] += float( row[-args.nAcc+i] )
+			num += 1
+
+		#end
+		if num != 0 :
+			for j in range(args.nAcc) : current[-args.nAcc +j] = acc[j]/num
+			writer.writerow(current)
+
--- Stealer/testParam.sh
+++ Stealer/testParam.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 _ratio=(0.01 0.02 0.03 0.05 0.1 0.2 0.5 0.7 0.9)
-_lr=(1e-5 3e-5 5e-5 7e-5 1e-4)
-_nb=(50 100)
+_lr=(1e-5 5e-5 1e-4) #_lr=(1e-5 3e-5 5e-5 7e-5 1e-4)
+_nb=(100) #_nb=(50 100)
 _epoch=(50 100 200)
-_loss=(BCELoss BCEWithLogitsLoss MSELoss)
-testTime=10
+_loss=(BCELoss MSELoss) #_loss=(BCELoss BCEWithLogitsLoss MSELoss)
+testTime=20
+outfile="${2}_${1%.*}_${3}_result.csv"
 
-echo training size,testing size,loss function,batch size,learning rate,epoch,control trainning accuracy,copy trainning accuracy,control testing accuracy,copy testing accuracy
+title="training size,testing size,loss function,batch size,learning rate,epoch,control trainning accuracy,copy trainning accuracy,control testing accuracy,copy testing accuracy"
+
+#echo -e "${1},${2},\c" | tee ${outfile}
+#echo `date "+%Y-%m-%d %H:%M:%S"` | tee -a ${outfile} 
+echo -e "${title}" | tee  ${outfile}
 
 for ratio in ${_ratio[@]}; do
     for loss in ${_loss[@]}; do
@@ -14,10 +19,13 @@ for ratio in ${_ratio[@]}; do
             for lr in ${_lr[@]}; do
                 for e in ${_epoch[@]}; do
                     for (( i = 0; i < $testTime; i++ )); do
-                        python ${1} ${2} -r ${ratio} -nb ${nb} -e ${e} -lr ${lr} -noP
+                        python ${1} ${2} -r ${ratio} -nb ${nb} -e ${e} -lr ${lr} -loss ${loss} -noP 2>&1 | tee -a ${outfile}
                     done
                 done
             done
         done
     done
 done
+
+#echo -e "${1},${2},\c" | tee  -a ${outfile}
+#echo `date "+%Y-%m-%d %H:%M:%S"` | tee -a ${outfile}
